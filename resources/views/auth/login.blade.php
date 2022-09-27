333ee79138475d7b8e1c774baf7994730ddcb947
Zawad <refatkhan93@gmail.com>
2022-09-27 19:43:30 +0800

    login warning icon added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -89,7 +89,9 @@
         </div>
         <div class="row">
             <div class="col mb-3">
-                <div class="alert alert-warning">Because the security of your information is paramount, we have incorporated this captcha
+                <div class="alert alert-warning">
+                    <span class="fas fa-fw fa-info-circle"></span>
+                    Because the security of your information is paramount, we have incorporated this captcha
                     to help protect our information.</div>
             </div>
         </div>
