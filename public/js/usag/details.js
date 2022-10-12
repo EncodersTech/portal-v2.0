8315b749a96c5dbcae5bf2404d663cac431dc887
Zawad <refatkhan93@gmail.com>
2022-10-12 23:27:05 +0800

    test

--- public/js/usag/details.js
+++ public/js/usag/details.js
@@ -3500,6 +3500,7 @@ function _asyncToGenerator(fn) { return function () { var self = this, args = ar
         for (var i in l.athletes) {
           if (l.athletes.hasOwnProperty(i)) {
             var athlete = l.athletes[i];
+            console.log(athlete);
             this.calculateAthleteFee(l, athlete, true);
           }
         }
