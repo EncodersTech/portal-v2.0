c75efd1bfc701bf071be97bed8b9b080288230a2
Zawad <refatkhan93@gmail.com>
2023-10-24 01:33:49 +0800

    update minor bugs on competitor text and meet edit athlete add

--- resources/js/components/Registration/edit/RegistrationEditPayment.vue
+++ resources/js/components/Registration/edit/RegistrationEditPayment.vue
@@ -617,6 +617,7 @@
                 let sum_h_p = this.summary.handling + this.summary.processor;
                 var flg = 0;
                 if (sum_h_p > 0) {
+                    let totalsave = 0;
                     this.summary.saving += 'Saved ' ;
                     for(let key in this.competitions){
                         let values = this.competitions[key];
@@ -626,12 +627,16 @@
                         let _saved_total_fee = (this.summary.subtotal * _sf) / 100; 
                         if(_saved_total_fee > sum_h_p)
                         {
-                        this.summary.saving += '$'+(_saved_total_fee - sum_h_p).toFixed(2) + ' than ' + key +',';
-                        flg = 1;
+                            totalsave += _saved_total_fee - sum_h_p;
+                            // this.summary.saving += '$'+(_saved_total_fee - sum_h_p).toFixed(2) + ' than ' + key +',';
+                            flg = 1;
                         }
                     }
-                    if(flg == 1)
-                    this.summary.saving = this.summary.saving.slice(0, -1);
+                    if(flg == 1 && totalsave > 0)
+                    {
+                        this.summary.saving += '$'+totalsave.toFixed(2) + ' compared to competitors';
+                        // this.summary.saving = this.summary.saving.slice(0, -1);
+                    }
                     else
                     this.summary.saving = '';
                 }
